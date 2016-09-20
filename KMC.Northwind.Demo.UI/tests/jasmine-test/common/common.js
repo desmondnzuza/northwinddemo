09605063d6e77c73016bd05353caf4e103400723
desmondnzuza <desmondnzuza@gmail.com>
2016-09-20 22:34:22 +0200

    prepares for client side unit testing

---
+++ KMC.Northwind.Demo.UI/tests/jasmine-test/common/common.js
@@ -0,0 +1,14 @@
+﻿var apiUrl = 'http://localhost:59192/api/';
+var appName = 'app';
+
+var failTest = function (error) {
+    expect(error).toBeUndefined();
+};
+
+var verifyErrorExits = function (error) {
+    expect(error).toBeUndefined();
+}
+
+var passTheTest = function () {
+    expect(1).toBe(1);
+};
